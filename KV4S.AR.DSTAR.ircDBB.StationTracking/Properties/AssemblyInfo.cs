c90aed35fbb7a9d73e5b770298ffa6ffb238acdf
RussellThomas <RussellThomas@CYBERPOWERPC>
2019-05-23 10:46:38 -0500

    Updated name in a few places

--- KV4S.AR.DSTAR.ircDBB.StationTracking/Properties/AssemblyInfo.cs
+++ KV4S.AR.DSTAR.ircDBB.StationTracking/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DSTAR.StationTracking")]
+[assembly: AssemblyTitle("DSTAR.ircDBB.StationTracking")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DSTAR.StationTracking")]
+[assembly: AssemblyCompany("KV4S")]
+[assembly: AssemblyProduct("DSTAR.ircDBB.StationTracking")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
